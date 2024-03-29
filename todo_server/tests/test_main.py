015be2971ec0d37412a2cf641287ae737c858056
Malik Shaqas Shahid <140010144+shaqas86@users.noreply.github.com>
2024-03-30 01:57:33 +0500

    3rd test

--- todo_server/tests/test_main.py
+++ todo_server/tests/test_main.py
@@ -35,3 +35,21 @@ def test_write_main():
        assert response.status_code == 200
        assert data["content"] == todo_content
 
+def test_read_todos():
+    connection_string=str(settings.TEST_DATABASE_URL).replace(
+       "postgresql", "postgresql+psycopg"
+   )
+
+    SQLModel.metadata.create_all(create_engine(connection_string))
+    with Session(create_engine(connection_string)) as session:
+
+        def get_session_override():
+            return session
+
+        app.dependency_overrides[get_session] = get_session_override
+        client = TestClient(app=app)
+
+        response = client.get("/todos/")
+
+        assert response.status_code == 200
+        
