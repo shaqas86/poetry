d1d354f88e0e15e573a49e37c225fc0f8286d77e
Malik Shaqas Shahid <140010144+shaqas86@users.noreply.github.com>
2024-04-22 00:20:00 +0500

    run with Docker

--- todo_server/tests/test_main.py
+++ todo_server/tests/test_main.py
@@ -1,69 +1,141 @@
-from http import client
 from fastapi.testclient import TestClient
-from sqlmodel import Session, select, create_engine,SQLModel,Field
-from todo_server.main import engine
-import pytest
+from sqlmodel import Field, Session, SQLModel, create_engine, select
 
-from todo_server.main import app,get_session, Todo
+# https://sqlmodel.tiangolo.com/tutorial/fastapi/tests/#override-a-dependency
+from todo_server.main import app, get_session, Todo
+
 from todo_server import settings
 
+# https://fastapi.tiangolo.com/tutorial/testing/
+# https://realpython.com/python-assert-statement/
+# https://understandingdata.com/posts/list-of-python-assert-statements-for-unit-tests/
+
+# postgresql://ziaukhan:oSUqbdELz91i@ep-polished-waterfall-a50jz332.us-east-2.aws.neon.tech/neondb?sslmode=require
 
 def test_read_main():
     client = TestClient(app=app)
     response = client.get("/")
     assert response.status_code == 200
-    assert response.json() == {"message": "Hello World"}
+    assert response.json() == {"Hello": "World"}
 
 def test_write_main():
-   
-   connection_string=str(settings.TEST_DATABASE_URL).replace(
-       "postgresql", "postgresql+psycopg"
-   )
-   engine=create_engine(connection_string)
 
-   SQLModel.metadata.create_all(engine)
-   print("creating table ...")
-#     with Session(engine) as session:
-#       t1 = Todo(title="todo1", description="desc1")
-#       t2 = Todo(title="todo2", description="desc2")
+    connection_string = str(settings.TEST_DATABASE_URL).replace(
+    "postgresql", "postgresql+psycopg")
+
+    engine = create_engine(
+        connection_string, connect_args={"sslmode": "require"}, pool_recycle=300)
+
+    SQLModel.metadata.create_all(engine)  
+
+    with Session(engine) as session:  
+
+        def get_session_override():  
+                return session  
+
+        app.dependency_overrides[get_session] = get_session_override 
+
+        client = TestClient(app=app)
+
+        todo_content = "buy bread"
+
+        response = client.post("/todos/",
+            json={"content": todo_content}
+        )
+
+        data = response.json()
+
+        assert response.status_code == 200
+        assert data["content"] == todo_content
+
+def test_read_list_main():
+
+    connection_string = str(settings.TEST_DATABASE_URL).replace(
+    "postgresql", "postgresql+psycopg")
+
+    engine = create_engine(
+        connection_string, connect_args={"sslmode": "require"}, pool_recycle=300)
+
+    SQLModel.metadata.create_all(engine)  
+
+    with Session(engine) as session:  
+
+        def get_session_override():  
+                return session  
+
+        app.dependency_overrides[get_session] = get_session_override 
+        client = TestClient(app=app)
+
+        response = client.get("/todos/")
+        assert response.status_code == 200
+    
+
+
+# from http import client
+# from fastapi.testclient import TestClient
+# from sqlmodel import Session, select, create_engine,SQLModel,Field
+# from todo_server.main import engine
+# import pytest
+
+# from todo_server.main import app,get_session, Todo
+# from todo_server import settings
+
+# def test_read_main():
+#     client = TestClient(app=app)
+#     response = client.get("/")
+#     assert response.status_code == 200
+#     # assert response.json() == {"message": "Hello World"}
+
+# def test_write_main():
+   
+#    connection_string=str(settings.TEST_DATABASE_URL).replace(
+#        "postgresql", "postgresql+psycopg"
+#    )
+#    engine=create_engine(connection_string)
+
+#    SQLModel.metadata.create_all(engine)
+#    print("creating table ...")
+#    with Session(engine) as session:
+#       t1 = Todo(title="todo1")
+#       t2 = Todo(title="todo2")
 #       session.add(t1)
 #       session.add(t2)
 #       session.commit()
      
     
    
-# engine=create_engine(connection_string)
-# SQLModel.metadata.create_all(engine)
-# with Session(engine) as session:
+# # engine=create_engine(connection_string)
+# # SQLModel.metadata.create_all(engine)
+# # with Session(engine) as session:
        
-#        def get_session_override():
-#            return session
+# #        def get_session_override():
+# #            return session
        
-#        app.dependency_overrides[get_session] = get_session_override
-#        client = TestClient(app=app)
+# #        app.dependency_overrides[get_session] = get_session_override
+# #        client = TestClient(app=app)
 
-#        todo_content = {"content": "test"}   
-#        response = client.post("/todo/",json=todo_content)
+# #        todo_content = {"content": "test"}   
+# #        response = client.post("/todo/",json=todo_content)
 
-#        data=response.json()
-#        assert response.status_code == 200
-#        assert data["content"] == todo_content
+# #        data=response.json()
+# #        assert response.status_code == 200
+# #        assert data["content"] == todo_content
 
-# def test_read_todos():
-#     connection_string=str(settings.TEST_DATABASE_URL).replace(
-#        "postgresql", "postgresql+psycopg"
-#    )
+# # def test_read_todos():
+# #     connection_string=str(settings.TEST_DATABASE_URL).replace(
+# #        "postgresql", "postgresql+psycopg"
+# #    )
 
-#     SQLModel.metadata.create_all(create_engine(connection_string))
-#     with Session(create_engine(connection_string)) as session:
+# #     SQLModel.metadata.create_all(create_engine(connection_string))
+# #     with Session(create_engine(connection_string)) as session:
 
-#         def get_session_override():
-#             return session
+# #         def get_session_override():
+# #             return session
 
-#         app.dependency_overrides[get_session] = get_session_override
-#         client = TestClient(app=app)
+# #         app.dependency_overrides[get_session] = get_session_override
+# #         client = TestClient(app=app)
 
-#         response = client.get("/todos/")
+# #         response = client.get("/todos/")
 
-#         assert response.status_code == 200
+# #         assert response.status_code == 200
         
--- todo_server/todo_server/main.py
+++ todo_server/todo_server/main.py
@@ -1,63 +1,141 @@
-from fastapi import FastAPI,Depends
-from sqlmodel import SQLModel, create_engine, Session, select,Field
-from todo_server import settings
+# main.py
 from contextlib import asynccontextmanager
-from typing import Union,Optional,Annotated
+from typing import Union, Optional, Annotated
+from todo_server import settings
+from sqlmodel import Field, Session, SQLModel, create_engine, select
+from fastapi import FastAPI, Depends
 
 
-#create class
-class Todo(SQLModel,table=True):
-    id:int = Field( default=None , primary_key=True)
-    title:str
-#create engine
+class Todo(SQLModel, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+    content: str 
 
-connection_str=str(settings.DATABASE_URL).replace("postgresql","postgresql+psycopg")
 
-engine=create_engine(connection_str)
+# only needed for psycopg 3 - replace postgresql
+# with postgresql+psycopg in settings.DATABASE_URL
+connection_string = str(settings.DATABASE_URL).replace(
+    "postgresql", "postgresql+psycopg"
+)
 
-def create_db_tables():
+
+# recycle connections after 5 minutes
+# to correspond with the compute scale down
+engine = create_engine(
+    connection_string, connect_args={"sslmode": "require"}, pool_recycle=300
+)
+
+
+def create_db_and_tables():
     SQLModel.metadata.create_all(engine)
 
+
+# The first part of the function, before the yield, will
+# be executed before the application starts.
+# https://fastapi.tiangolo.com/advanced/events/#lifespan-function
 @asynccontextmanager
-async def lifespan(app:FastAPI):
-    print("creating tables...")
-    create_db_tables()
+async def lifespan(app: FastAPI):
+    print("Creating tables..")
+    create_db_and_tables()
     yield
 
-app:FastAPI=FastAPI(lifespan=lifespan)
 
-#create session for dependancy injection
+app:FastAPI = FastAPI(lifespan=lifespan) 
+    
 def get_session():
     with Session(engine) as session:
         yield session
 
-@app.get("/")
-def index():
-    return {"message":"Hello World"}
 
-@app.post("/todo/", response_model=Todo)
-def create_todo(todo: Todo, session:Annotated[Session,Depends(get_session)]):
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+@app.post("/todos/", response_model=Todo)
+def create_todo(todo: Todo, session: Annotated[Session, Depends(get_session)]):
         session.add(todo)
         session.commit()
         session.refresh(todo)
         return todo
-#for update todo
-@app.put("/todo/{id}", response_model=Todo)
-def update_todo(id:int, todo:Todo, session:Annotated[Session, Depends(get_session)]):
-    todo_to_update=session.get(Todo, id)
-    todo_to_update.title=todo.title
-    session.commit()
-    session.refresh(todo_to_update)
-    return todo_to_update    
-#for delete todo
-@app.delete("/todo/{id}", response_model=Todo)
-def delete_todo(id:int, session:Annotated[Session, Depends(get_session)]):
-    todo_to_delete=session.get(Todo, id)
-    session.delete(todo_to_delete)
-    session.commit()
-    return todo_to_delete
-# get specific todo against todo ID
-@app.get("/todo/{id}", response_model=Todo)
-def get_todo(id:int, session:Annotated[Session, Depends(get_session)]):
-    todo=session.get(Todo, id)
-    return todo
+
+
+@app.get("/todos/", response_model=list[Todo])
+def read_todos(session: Annotated[Session, Depends(get_session)]):
+        todos = session.exec(select(Todo)).all()
+        return todos
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# from fastapi import FastAPI,Depends
+# from sqlmodel import SQLModel, create_engine, Session, select,Field
+# from todo_server import settings
+# from contextlib import asynccontextmanager
+# from typing import Union,Optional,Annotated
+
+
+# #create class
+# class Todo(SQLModel,table=True):
+#     id:int = Field( default=None , primary_key=True)
+#     title:str
+# #create engine
+
+# connection_str=str(settings.DATABASE_URL).replace("postgresql","postgresql+psycopg")
+
+# engine=create_engine(connection_str)
+
+# def create_db_tables():
+#     SQLModel.metadata.create_all(engine)
+
+# @asynccontextmanager
+# async def lifespan(app:FastAPI):
+#     print("creating tables...")
+#     create_db_tables()
+#     yield
+
+# app:FastAPI=FastAPI(lifespan=lifespan)
+
+# #create session for dependancy injection
+# def get_session():
+#     with Session(engine) as session:
+#         yield session
+
+# @app.get("/")
+# def index():
+#     return {"message":"Hello World"}
+
+# @app.post("/todo/", response_model=Todo)
+# def create_todo(todo: Todo, session:Annotated[Session,Depends(get_session)]):
+#         session.add(todo)
+#         session.commit()
+#         session.refresh(todo)
+#         return todo
+# #for update todo
+# @app.put("/todo/{id}", response_model=Todo)
+# def update_todo(id:int, todo:Todo, session:Annotated[Session, Depends(get_session)]):
+#     todo_to_update=session.get(Todo, id)
+#     todo_to_update.title=todo.title
+#     session.commit()
+#     session.refresh(todo_to_update)
+#     return todo_to_update    
+# #for delete todo
+# @app.delete("/todo/{id}", response_model=Todo)
+# def delete_todo(id:int, session:Annotated[Session, Depends(get_session)]):
+#     todo_to_delete=session.get(Todo, id)
+#     session.delete(todo_to_delete)
+#     session.commit()
+#     return todo_to_delete
+# # get specific todo against todo ID
+# @app.get("/todo/{id}", response_model=Todo)
+# def get_todo(id:int, session:Annotated[Session, Depends(get_session)]):
+#     todo=session.get(Todo, id)
+#     return todo
